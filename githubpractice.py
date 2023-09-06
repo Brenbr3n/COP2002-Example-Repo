50f9cd9b030a5bdc053fd0dfc5fa816357b65ee3
Brenbr3n <144075428+Brenbr3n@users.noreply.github.com>
2023-09-05 21:54:10 -0400

    Update githubpractice.py

--- githubpractice.py
+++ githubpractice.py
@@ -3,4 +3,5 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> # Creating a project for GitHub
 >>> # By Brennan Spradley
 >>> 
+>>> print("COP2002 Fall 2023")  
 print("I'm understanding how to use GitHub to keep track of changes!")
